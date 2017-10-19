2dc3938b9a3ca858c8b9999c3a85d44aea378ff2
chenfangming <chenfangming@91zdan.com>
2017-10-19 09:52:40 +0800

    1

--- rsa/rsa.go
+++ rsa/rsa.go
@@ -1,169 +1,94 @@
-package rsa
+package gorsa
 
 import (
-	"encoding/pem"
-	"encoding/base64"
-	"crypto/x509"
-	"crypto/rsa"
-	"crypto/rand"
-	"errors"
-	"crypto"
-	"io"
 	"bytes"
-	"encoding/asn1"
-	"fmt"
+	"crypto/rsa"
+	"errors"
+	"io/ioutil"
 )
 
-const (
-	CHAR_SET               = "UTF-8"
-	BASE_64_FORMAT         = "UrlSafeNoPadding"
-	RSA_ALGORITHM_KEY_TYPE = "PKCS8"
-	RSA_ALGORITHM_SIGN     = crypto.SHA256
-)
+var RSA = &RSASecurity{}
 
-type XRsa struct {
-	publicKey  *rsa.PublicKey
-	privateKey *rsa.PrivateKey
+type RSASecurity struct {
+	pubStr string          //公钥字符串
+	priStr string          //私钥字符串
+	pubkey *rsa.PublicKey  //公钥
+	prikey *rsa.PrivateKey //私钥
 }
 
-// 生成密钥对
-func CreateKeys(publicKeyWriter, privateKeyWriter io.Writer, keyLength int) error {
-	// 生成私钥文件
-	privateKey, err := rsa.GenerateKey(rand.Reader, keyLength)
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-	derStream := MarshalPKCS8PrivateKey(privateKey)
-	block := &pem.Block{
-		Type:  "PRIVATE KEY",
-		Bytes: derStream,
-	}
-	err = pem.Encode(privateKeyWriter, block)
-	if err != nil {
-		return err
-	}
-	// 生成公钥文件
-	publicKey := &privateKey.PublicKey
-	derPkix, err := x509.MarshalPKIXPublicKey(publicKey)
-	if err != nil {
-		return err
-	}
-	block = &pem.Block{
-		Type:  "PUBLIC KEY",
-		Bytes: derPkix,
-	}
-	err = pem.Encode(publicKeyWriter, block)
-	if err != nil {
-		return err
-	}
-	return nil
+// 设置公钥
+func (rsas *RSASecurity) SetPublicKey(pubStr string) (err error) {
+	rsas.pubStr = pubStr
+	rsas.pubkey, err = rsas.GetPublickey()
+	return err
 }
-func NewXRsa(publicKey []byte, privateKey []byte) (*XRsa, error) {
-	block, _ := pem.Decode(publicKey)
-	if block == nil {
-		return nil, errors.New("public key error")
-	}
-	pubInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
-	if err != nil {
-		return nil, err
-	}
-	pub := pubInterface.(*rsa.PublicKey)
-	block, _ = pem.Decode(privateKey)
-	if block == nil {
-		return nil, errors.New("private key error!")
-	}
-	priv, err := x509.ParsePKCS8PrivateKey(block.Bytes)
-	if err != nil {
-		return nil, err
-	}
-	pri, ok := priv.(*rsa.PrivateKey)
-	if ok {
-		return &XRsa{
-			publicKey:  pub,
-			privateKey: pri,
-		}, nil
-	} else {
-		return nil, errors.New("private key not supported")
-	}
+
+// 设置私钥
+func (rsas *RSASecurity) SetPrivateKey(priStr string) (err error) {
+	rsas.priStr = priStr
+	rsas.prikey, err = rsas.GetPrivatekey()
+	return err
+}
+
+// *rsa.PublicKey
+func (rsas *RSASecurity) GetPrivatekey() (*rsa.PrivateKey, error) {
+	return getPriKey([]byte(rsas.priStr))
+}
+
+// *rsa.PrivateKey
+func (rsas *RSASecurity) GetPublickey() (*rsa.PublicKey, error) {
+	return getPubKey([]byte(rsas.pubStr))
 }
 
 // 公钥加密
-func (r *XRsa) PublicEncrypt(data string) (string, error) {
-	partLen := r.publicKey.N.BitLen()/8 - 11
-	chunks := split([]byte(data), partLen)
-	buffer := bytes.NewBufferString("")
-	for _, chunk := range chunks {
-		bytes, err := rsa.EncryptPKCS1v15(rand.Reader, r.publicKey, chunk)
-		if err != nil {
-			return "", err
-		}
-		buffer.Write(bytes)
+func (rsas *RSASecurity) PubKeyENCTYPT(input []byte) ([]byte, error) {
+	if rsas.pubkey == nil {
+		return []byte(""), errors.New(`Please set the public key in advance`)
 	}
-	return base64.RawURLEncoding.EncodeToString(buffer.Bytes()), nil
+	output := bytes.NewBuffer(nil)
+	err := pubKeyIO(rsas.pubkey, bytes.NewReader(input), output, true)
+	if err != nil {
+		return []byte(""), err
+	}
+	return ioutil.ReadAll(output)
+}
+
+// 公钥解密
+func (rsas *RSASecurity) PubKeyDECRYPT(input []byte) ([]byte, error) {
+	if rsas.pubkey == nil {
+		return []byte(""), errors.New(`Please set the public key in advance`)
+	}
+	output := bytes.NewBuffer(nil)
+	err := pubKeyIO(rsas.pubkey, bytes.NewReader(input), output, false)
+	if err != nil {
+		return []byte(""), err
+	}
+	return ioutil.ReadAll(output)
+}
+
+// 私钥加密
+func (rsas *RSASecurity) PriKeyENCTYPT(input []byte) ([]byte, error) {
+	if rsas.prikey == nil {
+		return []byte(""), errors.New(`Please set the private key in advance`)
+	}
+	output := bytes.NewBuffer(nil)
+	err := priKeyIO(rsas.prikey, bytes.NewReader(input), output, true)
+	if err != nil {
+		return []byte(""), err
+	}
+	return ioutil.ReadAll(output)
 }
 
 // 私钥解密
-func (r *XRsa) PrivateDecrypt(encrypted string) (string, error) {
-	partLen := r.publicKey.N.BitLen() / 8
-	raw, err := base64.RawURLEncoding.DecodeString(encrypted)
-	chunks := split([]byte(raw), partLen)
-	buffer := bytes.NewBufferString("")
-	for _, chunk := range chunks {
-		decrypted, err := rsa.DecryptPKCS1v15(rand.Reader, r.privateKey, chunk)
-		if err != nil {
-			return "", err
-		}
-		buffer.Write(decrypted)
+func (rsas *RSASecurity) PriKeyDECRYPT(input []byte) ([]byte, error) {
+	if rsas.prikey == nil {
+		return []byte(""), errors.New(`Please set the private key in advance`)
 	}
-	return buffer.String(), err
-}
-
-// 数据加签
-func (r *XRsa) Sign(data string) (string, error) {
-	h := RSA_ALGORITHM_SIGN.New()
-	h.Write([]byte(data))
-	hashed := h.Sum(nil)
-	sign, err := rsa.SignPKCS1v15(rand.Reader, r.privateKey, RSA_ALGORITHM_SIGN, hashed)
+	output := bytes.NewBuffer(nil)
+	err := priKeyIO(rsas.prikey, bytes.NewReader(input), output, false)
 	if err != nil {
-		return "", err
+		return []byte(""), err
 	}
-	return base64.RawURLEncoding.EncodeToString(sign), err
-}
 
-// 数据验签
-func (r *XRsa) Verify(data string, sign string) error {
-	h := RSA_ALGORITHM_SIGN.New()
-	h.Write([]byte(data))
-	hashed := h.Sum(nil)
-	decodedSign, err := base64.RawURLEncoding.DecodeString(sign)
-	if err != nil {
-		return err
-	}
-	return rsa.VerifyPKCS1v15(r.publicKey, RSA_ALGORITHM_SIGN, hashed, decodedSign)
-}
-func MarshalPKCS8PrivateKey(key *rsa.PrivateKey) []byte {
-	info := struct {
-		Version             int
-		PrivateKeyAlgorithm []asn1.ObjectIdentifier
-		PrivateKey          []byte
-	}{}
-	info.Version = 0
-	info.PrivateKeyAlgorithm = make([]asn1.ObjectIdentifier, 1)
-	info.PrivateKeyAlgorithm[0] = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 1, 1}
-	info.PrivateKey = x509.MarshalPKCS1PrivateKey(key)
-	k, _ := asn1.Marshal(info)
-	return k
-}
-func split(buf []byte, lim int) [][]byte {
-	var chunk []byte
-	chunks := make([][]byte, 0, len(buf)/lim+1)
-	for len(buf) >= lim {
-		chunk, buf = buf[:lim], buf[lim:]
-		chunks = append(chunks, chunk)
-	}
-	if len(buf) > 0 {
-		chunks = append(chunks, buf[:len(buf)])
-	}
-	return chunks
+	return ioutil.ReadAll(output)
 }
